bdb141627e2cc6cf8d03b2583dff57ff4f509a42
Borna <borna.jamali@gmail.com>
2022-04-19 10:25:20 +0300

    Added transpose,determinant, inverse logic for MatrixLogic class

--- src/index.py
+++ src/index.py
@@ -1,11 +1,13 @@
 
 from ui.ui import ui
-
+from matrixcalculator.matrixlogic import MatrixLogic
 
 def main():
     start = ui()
-
-    start.start()
+    s = MatrixLogic()
+    matrix = [[6,1,1], [4,-2,5], [2,8,7]]
+    print(s.matrix_inverse(matrix))
+    start.start()   
 
 
 main()
--- src/matrixcalculator/matrixlogic.py
+++ src/matrixcalculator/matrixlogic.py
@@ -1,12 +1,12 @@
+import numpy as np
+class MatrixLogic:
 
-'''
+    '''
     This class has functions to calculate operations between two matrices.
     Such as,
-    multiplication, addition and substraction    
-'''
-
-
-class MatrixLogic:
+    multiplication, addition and substraction. It can also produce a inverse, determinant or transpose of a matrix
+    '''
+    #TODO Transpose, inverse,determinant
 
     # Addition of the two matrices.
     def matrix_addition(self, matrix1, matrix2):
@@ -41,3 +41,34 @@ class MatrixLogic:
                     else:
                         return False
         return final_matrix
+
+    #Find the determinant by cofactor 
+    def matrix_determinant(self,matrix):
+        matrix_a = matrix[0][0]
+        matrix_b = matrix[0][1]
+        matrix_c = matrix[0][2]
+
+        determinant_a = matrix_a*(matrix[1][1]*matrix[2][2] - matrix[1][2]*matrix[2][1])
+        determinant_b = -matrix_b*(matrix[1][0]*matrix[2][2] - matrix[1][2]*matrix[2][0])
+        determinant_c = matrix_c*(matrix[1][0]*matrix[2][1] - matrix[2][0]*matrix[1][1])
+        
+        answer = determinant_a+determinant_b+determinant_c
+        return answer
+    
+    #Transpose the matrix rows = cols 
+    def matrix_transpose(self,matrix):
+        matrix_transpose = [[1 for i in range(3)] for j in range(3)]
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                matrix_transpose[j][i] = matrix[i][j]
+
+        return matrix_transpose
+    
+    def matrix_inverse(self,matrix):
+        if self.matrix_determinant(matrix) == 0: 
+            return -1
+
+        create_matrix = np.array(matrix)
+        inverse_matrix = np.linalg.inv(create_matrix)
+        return inverse_matrix
